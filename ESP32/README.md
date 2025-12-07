e208e93331618baa3e730dceb0b8922a2e4cd1b2
FurWaz <fur.waz06@gmail.com>
2025-12-07 10:41:16 +0100

    Removed ESP32 folder (old firmware)

--- ESP32/README.md
+++
@@ -1,4 +0,0 @@
-# TNY - 360 ESP32
-
-This is the source code for the [TNY - 360](https://tny-robotics.com/robots/tny-360) robot' ESP32 microcontroller.
-The code is written in C++ and uses the [ESP-IDF](https://docs.espressif.com/projects/esp-idf/en/stable/esp32s3/get-started/index.html) framework.
--- ESP32/include/gpio_buttons.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <driver/gpio.h>
-
-esp_err_t gpio_buttons_init(gpio_num_t left, gpio_num_t right);
-esp_err_t gpio_buttons_scan();
-
-void gpio_buttons_set_cb(void(*onLeftPressed)(), void(*onLeftLongPressed)(), void(*onRightPressed)(), void(*onRightLongPressed)());
