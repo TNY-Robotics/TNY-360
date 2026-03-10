c69a20767322a97385876ef356b7057f8ed8672c
Paul LOISIL <fur.waz06@gmail.com>
2026-03-10 14:27:35 +0100

    Pushed SineProvider additions

--- Firmware/include/audio/SineProvider.hpp
+++ Firmware/include/audio/SineProvider.hpp
@@ -7,11 +7,13 @@ class SineProvider : public SoundProvider
 public:
     SineProvider();
 
+    SineProvider(float frequencyHz, float volume);
+
     bool provideSamples(Speaker::Sample* buffer, size_t sampleCount) override;
 
 private:
     float volume = 1.0f;
-    float frequency = 880.0f;
+    float frequency = 440.0f;
     float phase = 0.0f;
     float phaseIncrement = 0.0f;
 };
